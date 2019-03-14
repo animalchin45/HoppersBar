7503ee6a296f87812cec185323ee5bd5cad4ca42
Tim O'Brien <timobrien.inc@gmail.com>
2019-03-14 19:21:34 -0400

    dsjfal

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var indexRoute = require("./routes/index");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use("/gsap", express.static(__dirname + "/node_modules/gsap"));
-app.use("/scrollmagic", express.static(__dirname = "/node_modules/scrollmagic"));
 
 app.use(indexRoute);
 
